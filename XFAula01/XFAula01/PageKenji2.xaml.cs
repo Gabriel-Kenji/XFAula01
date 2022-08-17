4f087ed5f97eefe0031db62e5101efabac33164c
Gabriel Kenji Utiyama <gabrielkenji2000@hotmail.com>
2022-08-17 17:17:04 -0300

    .

---
+++ XFAula01/XFAula01/PageKenji2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFAula01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageKenji2 : ContentPage
+    {
+        public PageKenji2()
+        {
+            InitializeComponent();
+        }
+    }
+}
